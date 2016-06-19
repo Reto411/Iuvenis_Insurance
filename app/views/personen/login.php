80c2bed790264e87e90fae26726bc1d90bf39694
Aloisli Bürgisser <BuergisserA@EB.local>
2016-06-19 14:43:08 +0200

    Make a login form

--- app/views/personen/login.php
+++ app/views/personen/login.php
@@ -1,1 +1,8 @@
-Hallo Login
+Sie müssen sich zuerst einloggen.
+
+<form>
+	<label>Vorname: </label><input type="text" name="prename"><br/>
+	<label>Nachname: </label><input type="text" name="name"><br/>
+	<label>Password: </label><input type="password" name="password"><br/>
+	<button type="submit">Login</button>
+</form>
