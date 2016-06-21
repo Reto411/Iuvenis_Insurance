ab5f6ec1bc2492db8eb6fc68a807939ce0f4a7fd
RogerMetzger <roger.metzger@hotmail.com>
2016-06-21 14:07:58 +0200

    Transaktion hinzugefügt & SQL Injection sicher gemacht

--- connection.php
+++ connection.php
@@ -8,11 +8,11 @@
 
 		public static function getInstance() {
 			if(!isset(self::$instance)) {
-				// create db connection
-				$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-				
-				//self::$instance = new PDO('mysql:MYSQLSERVER', 'root', '', $pdo_options);
-				self::$instance = new PDO('mysql:dbname=iuvenis_insurance_db;host=localhost;charset=utf8', 'root', '', $pdo_options);
+				// create db connection				
+				//self::$instance = new PDO('mysql:MYSQLSERVER', 'root', '');
+				self::$instance = new PDO('mysql:dbname=iuvenis_insurance_db;host=localhost;charset=utf8', 'root', '');
+				self::$instance->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+				self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			}
 			return self::$instance;
 		}
